6921f3587f945053ba796d5a7941ef97245f03db
JoanBonell <joanbonellruiz@gmail.com>
2022-10-26 17:48:45 +0200

    modified:   handson/ho04/enunciat-act11/main.c

--- handson/ho04/enunciat-act11/main.c
+++ handson/ho04/enunciat-act11/main.c
@@ -9,6 +9,28 @@ main(int argc, char *argv[])
 {
     /*Feu un fork de un pare i un fill, i el fill ha de fer el recobriment de l'executable.*/
     //Creamos un proceso padre:
-    
+    pid = fork();
+    if(pid == -1){
+        exit(-1);
+    }
+    else if(pid == 0){
+        //Fill
+        run_child();
+    }
+    else{
+        //Pare
+        run_parent();
+    }
+
+}
+
+void run_child(){
+    init_pokedex();
+    if(){
+        
+    }
+}
+
+void run_parent(){
 
 }
