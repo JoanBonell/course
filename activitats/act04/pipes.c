a6cc18f878852c438ecb0cacc07c9c4ed99734d5
JoanBonell <joanbonellruiz@gmail.com>
2022-11-24 02:52:32 +0100

    suprimit:         ../act03/main.c 	suprimit:         ../act03/ossh.c 	modificat:        ../act03/shell.c 	suprimit:         ../act03/shell2.c 	modificat:        ../act04/pipes.c

--- activitats/act04/pipes.c
+++ activitats/act04/pipes.c
@@ -22,14 +22,15 @@ int main(int argc, char *argv[])
     int fd[2], fd1[2];
     FILE *fp;
     
-    fp = fopen("user.txt", "w");
+    fp = fopen("user.txt", "a");
+    FILE *fp2 = fopen("/etc/passwd", "r");
 
     int status;
     //Implement stdin and stdout < > and | pipes
     //https://www.youtube.com/watch?v=8LdQ09Ep9RY
 
     char *p1[] = {"grep", NULL}; 
-    char *p2[] = {"cat", "/etc/passwd", NULL}; 
+    //char *p2[] = {"cat", "/etc/passwd", NULL}; 
     char *p3[] = {"$(whoami)", NULL};
 
     if (pipe(fd)<0){
@@ -74,14 +75,15 @@ int main(int argc, char *argv[])
         case 0:
             // Fill -> grep whoami -> llegeix stdin -> imprimeix a stdout
             // Tanquem stdout i redireccionem stdout a l'escriptura fd[1] de la pipe
-            dup2(fd[1],STDOUT_FILENO);
+            //dup2(fd[1],STDOUT_FILENO);
+            dup2(fileno(fp2),STDIN_FILENO);
             dup2(fileno(fp),STDOUT_FILENO);
             close(fd[1]);
             close(fd[0]);
             execvp(p1[0], p1);
             return EXIT_FAILURE;
     }
-    switch(pid3 = fork()){
+    /*switch(pid3 = fork()){
         case -1:
             perror("Error fork() - pid3");
             return EXIT_FAILURE;
@@ -93,7 +95,7 @@ int main(int argc, char *argv[])
             close(fd[0]);
             execvp(p2[0], p2);
             return EXIT_FAILURE;
-    }
+    }*/
     close(fd[0]);
     close(fd[1]);
     close(fd1[0]);
