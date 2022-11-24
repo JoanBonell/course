a9959862d2974cfb16bae41a31ccadb4ec662824
JoanBonell <joanbonellruiz@gmail.com>
2022-11-24 14:40:20 +0100

    Jugador.c

--- activitats/act05/EscapeRoom.c
+++ activitats/act05/EscapeRoom.c
@@ -21,21 +21,41 @@ int main(int argc, char *argv[]){
     int num_segons_partida = atoi(argv[1]); // Passem el string argv[2] a int i ho guardem a 'num_segons_partida'.
 
     int n_jugadores = atoi(argv[2]);
-    pid_t pid;
+    pid_t * pids = malloc(sizeof(pid_t) * n_jugadores);
+
     for(int i = 0; i < n_jugadores; i++){
-        switch (pid)
+        pids[i] = fork();
+        switch (pids[i])
         {
         case 0:
             printf("EscapeRoom[%d] ha entrat el jugador[%d]\n", getppid(), getpid());
+            //dup2
+            //exec("jugador", NULL);
             break;
         case -1:
             perror("Error fork() - pid \n");
             break;
-        default:
-            pid = fork();
-            break;
-        }
+        }  
     }
+
+    
+    while(jugant==1){
+    // llegir de la pipe
+    // per cada paraula llegida strcmp(argv[3])
+    //
+    }
+
+    // Han guanyat
+    for(int i = 0; i < n_jugadores; i++){
+        kill(,SIGUS1);
+    }
+  
+
+    // Esperant que acabin
+    for(int i = 0; i < n_jugadores; i++){
+        waitpid(pids[i],0,0);
+    }
+
     
 
 }
--- activitats/act05/jugador.c
+++ activitats/act05/jugador.c
@@ -6,26 +6,52 @@
 #include <signal.h>
 #define LONGITUD_PASSWD 4
 void cadena_aleatoria_byUs(char **destino);
+void acabar();
+
 int main(int argc, char *argv[]){
     //crear paraula
-    char *paraula_aleatoria = malloc(LONGITUD_PASSWD * sizeof(char));
-
+    char* paraula_aleatoria = malloc(LONGITUD_PASSWD * sizeof(char));
     cadena_aleatoria_byUs(&paraula_aleatoria);
-    printf("%s", paraula_aleatoria);
+    printf("%s\n", paraula_aleatoria);fflush(stdout);
+
+    //crear paraula mode 2
+    char** paraules = malloc(1 * sizeof(char*));
+    cadena_aleatoria_byUs2(paraules);
+    printf("%s\n", paraules[0]);fflush(stdout);
     //definir senyal amb tractament
-
+    signal(SIGUSR1, acabar);
     // escriure a 1
-    while(1){}
-}
+    while(1){
 
-
-
-//Generar paraula aleatoria desde 'a' fins a 'z' tot minúscules.
-void cadena_aleatoria_byUs(char **destino){
-    srand(time(NULL));
-    char aleatorio;
-    for(int i = 0; i < LONGITUD_PASSWD;i++){
-        aleatorio = 'a' + rand() % (('z'-'a')+1);
-        *destino[i] = aleatorio;
     }
 }
+
+void acabar(){
+    exit(1);
+}
+
+//Generar paraula aleatoria desde 'a' fins a 'z' tot minúscules.
+void cadena_aleatoria_byUs(char ** destino){
+    srand(time(NULL));
+    char aleatorio;
+    char* word = malloc(LONGITUD_PASSWD * sizeof(char)); 
+    for(int i = 0; i < LONGITUD_PASSWD;i++){
+        aleatorio = 'a' + rand() % (('z'-'a')+1);
+        word[i] = aleatorio;
+    }
+    printf("%s\n", word);fflush(stdout);
+    *destino=word;
+}
+
+void cadena_aleatoria_byUs2(char ** destino){
+    srand(time(NULL));
+    char aleatorio;
+    char* word = malloc(LONGITUD_PASSWD * sizeof(char)); 
+    for(int i = 0; i < LONGITUD_PASSWD;i++){
+        aleatorio = 'a' + rand() % (('z'-'a')+1);
+        word[i] = aleatorio;
+    }
+    printf("%s\n", word);fflush(stdout);
+    destino[0]=word;
+}
+
